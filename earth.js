708dc00385e7da9de27e835f5d4ecbc06e8f8d41
Alicia McDaniel <alicia.mcdaniel@atlasschool.com>
2025-08-15 03:07:52 -0500

    css added

--- earth.js
+++ earth.js
@@ -1,1 +1,16 @@
 const API_KEY = 'kJNZ1Ze3U6Yd1JrePehO5LWlK0D9e2IWI9oDB7q8';
+
+document.getElementById('load-earth').addEventListener('click', () => {
+  const lat = document.getElementById('lat').value;
+  const lon = document.getElementById('lon').value;
+  const date = document.getElementById('date').value;
+
+  const url = `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=${date}&dim=0.1&api_key=${API_KEY}`;
+
+  fetch(url)
+    .then(res => res.blob())
+    .then(blob => {
+      const imgUrl = URL.createObjectURL(blob);
+      document.getElementById('earth-image').innerHTML = `<img src="${imgUrl}" alt="Earth Image">`;
+    });
+});
