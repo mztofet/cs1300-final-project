580ad0a3f883b8643f0c9bec3d39b446d6e39cda
Alicia McDaniel <alicia.mcdaniel@atlasschool.com>
2025-08-15 06:05:59 -0500

    updated earth data page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cs1300-final-project
 creating our own website together as a team 
+
+We decided to go with the prompt of interstellar 
--- earth.js
+++ earth.js
@@ -1,16 +1,59 @@
 const API_KEY = 'kJNZ1Ze3U6Yd1JrePehO5LWlK0D9e2IWI9oDB7q8';
 
-document.getElementById('load-earth').addEventListener('click', () => {
-  const lat = document.getElementById('lat').value;
-  const lon = document.getElementById('lon').value;
-  const date = document.getElementById('date').value;
+document.getElementById("load-epic").addEventListener("click", () => {
+  const date = document.getElementById("epic-date").value;
+  const epicUrl = `https://api.nasa.gov/EPIC/api/natural/date/${date}?api_key=${API_KEY}`;
 
-  const url = `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&date=${date}&dim=0.1&api_key=${API_KEY}`;
+  console.log("EPIC Date:", date);
 
-  fetch(url)
-    .then(res => res.blob())
-    .then(blob => {
-      const imgUrl = URL.createObjectURL(blob);
-      document.getElementById('earth-image').innerHTML = `<img src="${imgUrl}" alt="Earth Image">`;
+  fetch(epicUrl)
+    .then(res => res.json())
+    .then(data => {
+      const container = document.getElementById("earth-image");
+      container.innerHTML = ""; // Clear previous images
+
+      if (!data || data.length === 0) {
+        container.innerHTML = `<p>No EPIC images available for this date.</p>`;
+        return;
+      }
+
+      data.forEach(item => {
+        const imageName = item.image;
+        const dateParts = date.split("-");
+        const imageUrl = `https://epic.gsfc.nasa.gov/archive/natural/${dateParts[0]}/${dateParts[1]}/${dateParts[2]}/png/${imageName}.png`;
+        const img = document.createElement("img");
+        
+        
+        img.src = imageUrl;
+        img.alt = "EPIC Earth Image";
+        img.style.width = "300px";
+        img.style.margin = "10px";
+
+        container.appendChild(img);
+
+        img.addEventListener("click", () => {
+  window.open(img.src, "_blank");
+
+
+  img.addEventListener("click", () => {
+  window.open(img.src, "_blank"); // or modal logic
+});
+});
+      });
+    })
+    .catch(err => {
+      console.error("EPIC fetch error:", err);
+      document.getElementById("earth-image").innerHTML = `<p>Error loading EPIC images. Try a different date.</p>`;
     });
+});
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+
+img.addEventListener("click", () => {
+  modal.style.display = "flex";
+  modalImg.src = img.src;
+});
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
 });
